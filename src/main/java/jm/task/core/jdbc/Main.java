6afb7b39c7bb0509ce149f63f5a355b319b4c71c
Olga Bystrova <bystrovaolya@gmail.com>
2022-07-28 17:17:16 +0300

    Update

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -24,12 +24,17 @@ public class Main {
         key.saveUser(user3.getName(), user3.getLastName(), user3.getAge());
         key.saveUser(user4.getName(), user4.getLastName(), user4.getAge());
 
+//        List<User> users = key.getAllUsers();
+//        for (User i : users) {
+//            System.out.println(i);
+//        }
+
+        key.removeUserById(2);
+
         List<User> users = key.getAllUsers();
         for (User i : users) {
             System.out.println(i);
         }
-
-        key.removeUserById(1);
 
         key.cleanUsersTable();
 
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,18 +1,18 @@
 package jm.task.core.jdbc.util;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Util {
+    static String userName = "root";
+    static String password = "myroot123";
+    static String connectionURL = "jdbc:mysql://localhost:3306/my_db_113";
+    static Connection conn = null;
 
     public static Connection getSQLConnection() {
-        String userName = "root";
-        String password = "myroot123";
-        String connectionURL = "jdbc:mysql://localhost:3306/my_db_113";
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            return DriverManager.getConnection(connectionURL, userName,
+            return conn = DriverManager.getConnection(connectionURL, userName,
                     password);
         } catch (SQLException | ClassNotFoundException e) {
             System.out.println("Ошибка при подключении к БД!");
@@ -21,14 +21,9 @@ public class Util {
     }
 
     public static Connection closeSQLConnection() {
-        String userName1 = "root";
-        String password1 = "myroot123";
-        String connectionURL1 = "jdbc:mysql://localhost:3306/my_db_113";
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            DriverManager.getConnection(connectionURL1, userName1,
-                    password1).close();
-        } catch (SQLException | ClassNotFoundException e) {
+            conn.close();
+        } catch (SQLException e) {
             System.out.println("Ошибка при закрытии БД!");
         }
         return null;
